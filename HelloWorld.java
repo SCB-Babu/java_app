ea7cc5a1d09a9a44380266645f996cfdff28c22e
EC2 Default User <ec2-user@ip-172-31-10-198.ap-south-1.compute.internal>
2021-12-11 05:47:08 +0000

    added2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
-	System.out.println("Hello, World + Jenkins + Devops + Docker+ 2!");
+	System.out.println("Hello, World + Jenkins + Devops + Docker!");
      	
  }
 }
