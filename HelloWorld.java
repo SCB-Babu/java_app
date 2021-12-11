f4f98feae40d650a173c95364ba5e3edd1a93aba
EC2 Default User <ec2-user@ip-172-31-10-198.ap-south-1.compute.internal>
2021-12-11 06:38:41 +0000

    Webhooks1

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
-	System.out.println("Hello, World + Jenkins + Devops + Dockeir + 1234!");
+	System.out.println("Hello, World + Jenkins + Devops + Dockeir + 1234567!");
      	
  }
 }
